3ba02df19bb1e84d69b145a5bf8b2c5219508e6d
DSM2 <fatec-dsm2@academico.intra>
2025-08-07 19:28:16 -0300

    @dvnxfx

---
+++ javascript/01.1/js-01.1.js
@@ -0,0 +1,57 @@
+// TIPOS DE FUNÇÕES
+
+//função simples
+function showMessage(){
+    document.write("<p>Esta mensagem é exibida usando uma função simples</p>");
+}
+
+//invocando a função;
+showMessage();
+
+//função com parâmetros;
+const message = "Janeiro";
+const year = 2023;
+function userMessage(a){
+    document.write("<p>O parâmetro usado na função é: " + a + "</p>"); //concatenação
+}
+userMessage(message); //argumento
+function userMessage2(b){
+    document.write(`<p>O parâmetro usado na função é: ${b} </p>`); //template string ou literal string
+}
+userMessage2(year); //argumento
+
+// variáveis podem ser declaradas de 3 formas diferentes;
+// VAR: melhor evitar, pois é um modo antigo de declarar variáveis e é global, podendo causar problemas de escopo/segurança;
+var nome = "João";
+var nome = "Maria";
+
+// LET: modo de variável local, ou seja, acessível apenas dentro do escopo onde foi declarada - utilize quando for necessário reatribuir o valor da variável;
+// declaração != atribuição;
+let idade = 30;
+
+// CONST: variável constante, ou seja, não pode ser alterada após a declaração (não pode ser reatribuídam, mas sempre deve ser inicializado na declaração); 
+const cidade = "São Paulo";
+//const cidade = "Registro";
+
+
+// FUNÇÃO SIMPLES
+function showMessage(){
+        document.write("<h2>Olá! Bem vindo! Essa é sua primeira função!</h2>")
+}
+
+function showMessage(){
+        document.write(message);
+}
+// Invocando a função:
+showMessage();
+
+//FUNÇÃO COM PARÂMETROS
+const user = "Dani Fiel";
+
+function userMessage(user) {
+// Essa função recebe um parâmetro 
+document.write(`<h3>O que deseja fazer hoje, ${user} ?</h3>`);
+// ${} -> Template Strings / Literal Strings 
+// É usado para inserir variáveis dentro de STRINGS (CRASE)
+}
+userMessage(user); // ARGUMENTO
