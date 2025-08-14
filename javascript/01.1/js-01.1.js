9f135828f0f8373546303b67d12a764a3f579675
DSM2 <fatec-dsm2@academico.intra>
2025-08-13 22:38:36 -0300

    @dvnxfx

--- javascript/01.1/js-01.1.js
+++ javascript/01.1/js-01.1.js
@@ -1,8 +1,8 @@
 // TIPOS DE FUNÇÕES
 
 //função simples
-function showMessage(){
-    document.write("<p>Esta mensagem é exibida usando uma função simples</p>");
+function showMessage() {
+  document.write("<p>Esta mensagem é exibida usando uma função simples</p>");
 }
 
 //invocando a função;
@@ -11,12 +11,12 @@ showMessage();
 //função com parâmetros;
 const message = "Janeiro";
 const year = 2023;
-function userMessage(a){
-    document.write("<p>O parâmetro usado na função é: " + a + "</p>"); //concatenação
+function userMessage(a) {
+  document.write(`<p>O parâmetro usado na função é: ${a}</p>`); //concatenação
 }
 userMessage(message); //argumento
-function userMessage2(b){
-    document.write(`<p>O parâmetro usado na função é: ${b} </p>`); //template string ou literal string
+function userMessage2(b) {
+  document.write(`<p>O parâmetro usado na função é: ${b} </p>`); //template string ou literal string
 }
 userMessage2(year); //argumento
 
@@ -29,29 +29,64 @@ var nome = "Maria";
 // declaração != atribuição;
 let idade = 30;
 
-// CONST: variável constante, ou seja, não pode ser alterada após a declaração (não pode ser reatribuídam, mas sempre deve ser inicializado na declaração); 
+// CONST: variável constante, ou seja, não pode ser alterada após a declaração (não pode ser reatribuídam, mas sempre deve ser inicializado na declaração);
 const cidade = "São Paulo";
 //const cidade = "Registro";
 
+// // FUNÇÃO SIMPLES
+// function showMessage() {
+//   document.write("<h2>Olá! Bem vindo! Essa é sua primeira função!</h2>");
+// }
 
-// FUNÇÃO SIMPLES
-function showMessage(){
-        document.write("<h2>Olá! Bem vindo! Essa é sua primeira função!</h2>")
-}
+// function showMessage() {
+//   document.write(message);
+// }
+// // Invocando a função:
+// // showMessage();
 
-function showMessage(){
-        document.write(message);
-}
-// Invocando a função:
-showMessage();
 
 //FUNÇÃO COM PARÂMETROS
 const user = "Dani Fiel";
 
 function userMessage(user) {
-// Essa função recebe um parâmetro 
-document.write(`<h3>O que deseja fazer hoje, ${user} ?</h3>`);
-// ${} -> Template Strings / Literal Strings 
-// É usado para inserir variáveis dentro de STRINGS (CRASE)
+  // 'user' -> parâmetro
+  // Essa função recebe um parâmetro
+  document.write(`<h3>O que deseja fazer hoje, ${user} ?</h3>`);
+  // ${} -> Template Strings / Literal Strings
+  // É usado para inserir variáveis dentro de STRINGS (CRASE)
 }
 userMessage(user); // ARGUMENTO
+
+//FUNÇÃO COM MAIS DE UM PARÂMETRO
+const n1 = 10;
+const n2 = 12;
+
+function mult(n1, n2) {
+  // essa função recebe dois parâmetros
+  let result = n1 * n2;
+  document.write(`A multiplicação de ${n1} e ${n2} ${result}.`);
+}
+mult(n1, n2);
+
+// FUNÇÃO COM RETORNO
+const num1 = 1000;
+const num2 = 5;
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+document.write(
+  `<p>A divisão de ${num1} por ${num2} é igual a ${div(num1, num2)}.</p>`
+);
+
+// FUNÇÃO COM DIFERENTES RETORNOS
+const number = 7;
+
+function parImpar(number) {
+  if (number % 2 == 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
+}
+document.write(`<p>O número ${number} é <strong>${parImpar(number)}</strong>`)
